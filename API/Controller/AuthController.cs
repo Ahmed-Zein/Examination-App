3412b5dd3f09356e205446efe642c95b5f15487a
Ahmed-Zein <ahmedzein3652@gmail.com>
2024-12-03 23:39:00 +0200

    add: Auth controller

--- API/Controller/AuthController.cs
+++ API/Controller/AuthController.cs
@@ -11,22 +11,19 @@ public class AuthController(IAuthService authService) : ControllerBase
     [HttpPost("signup")]
     public async Task<ActionResult<AuthenticationResponseDto>> Signup([FromBody] RegisterRequestDto registerRequestDto)
     {
-        // flow:
-        // we need a Signup DTO, Response
-        // the infra service takes that dto
-        // what happens in the infra stays in the infra xd
-        // ... create a user model
-        // ... add role to user
-        // ... return the user
-        // if what is returned is ok return status
-        var res = await authService.Register(registerRequestDto);
-        return res.IsSuccess ? Ok(res.Value) : BadRequest(res.Errors);
+        var authResult = await authService.Register(registerRequestDto);
+        return authResult.IsSuccess
+            ? Ok(authResult.Value)
+            : BadRequest(new AuthenticationResponseDto()
+                { Success = false, Errors = authResult.Errors.Select(e => e.Message).ToList() });
     }
 
     [HttpPost("login")]
-    public Task<ActionResult<AuthenticationResponseDto>> Login([FromBody] LoginDto loginDto)
+    public async Task<ActionResult<AuthenticationResponseDto>> Login([FromBody] LoginRequestDto loginRequestDto)
     {
-        return Task.FromResult<ActionResult<AuthenticationResponseDto>>(
-            Ok(new AuthenticationResponseDto { Token = "", message = "hola", success = true }));
+        var authResult = await authService.Login(loginRequestDto);
+        return authResult.IsSuccess
+            ? Ok(authResult.Value)
+            : BadRequest(authResult.Value);
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -7,12 +7,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -40,8 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapControllers();
-
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
